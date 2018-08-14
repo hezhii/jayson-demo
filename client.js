a0a9176923a9b7891df96df9a02ba92b905dddfb
biu <hezhou920@gmail.com>
2018-08-14 11:24:28 +0800

    notification

--- client.js
+++ client.js
@@ -8,3 +8,11 @@ client.request('add', [1, 1], function (err, res) {
   }
   console.log('call "add" with params: [1, 1], result:', res.result);
 })
+
+// the third parameter is set to "null" to indicate a notification
+client.request('ping', null, null, function (err, res) {
+  if (err) {
+    throw err;
+  }
+  console.log('ping');
+})
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = 8080;
 const server = jayson.server({
   add: function (args, callback) {
     callback(null, args[0] + args[1]);
+  },
+  ping: function (args, callback) {
+    console.log(`[${new Date().toLocaleString()}] - Received client ping`)
   }
 })
 
